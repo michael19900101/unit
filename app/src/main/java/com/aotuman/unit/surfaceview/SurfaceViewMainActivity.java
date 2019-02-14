72e79daf8700b805add5368a5c3d965c878f0eac
huangkunting <huangkunting@wxchina.com>
2019-02-14 16:09:17 +0800

    增加surfaceview demo

---
+++ app/src/main/java/com/aotuman/unit/surfaceview/SurfaceViewMainActivity.java
@@ -0,0 +1,15 @@
+package com.aotuman.unit.surfaceview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aotuman.unit.R;
+
+public class SurfaceViewMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surface_main);
+    }
+}
---
+++ app/src/main/java/com/aotuman/unit/surfaceview/SurfaceViewTemplate.java
@@ -0,0 +1,86 @@
+package com.aotuman.unit.surfaceview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * SurfaceView 模板代码，唯一的不同就是绘制逻辑的不同
+ * (1) 通过lockCanvas()方法获得Canvas对象
+ * (2) 在子线程中使用Canvas对象进行绘制
+ * (3) 使用unlockCanvasAndPost()方法将画布内容进行提
+ */
+public class SurfaceViewTemplate extends SurfaceView implements SurfaceHolder.Callback, Runnable{
+    private SurfaceHolder mSurfaceHolder;
+    //绘图的Canvas
+    private Canvas mCanvas;
+    //子线程标志位
+    private boolean mIsDrawing;
+    public SurfaceViewTemplate(Context context) {
+        this(context, null);
+    }
+
+    public SurfaceViewTemplate(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SurfaceViewTemplate(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mIsDrawing = true;
+        //开启子线程
+        new Thread(this).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mIsDrawing = false;
+    }
+
+    @Override
+    public void run() {
+        while (mIsDrawing){
+            drawSomething();
+        }
+    }
+    //绘图逻辑
+    private void drawSomething() {
+        try {
+            //获得canvas对象
+            mCanvas = mSurfaceHolder.lockCanvas();
+            //绘制背景
+            mCanvas.drawColor(Color.WHITE);
+            //绘图
+        }catch (Exception e){
+
+        }finally {
+            if (mCanvas != null){
+                //释放canvas对象并提交画布
+                mSurfaceHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
+    }
+
+    /**
+     * 初始化View
+     */
+    private void initView(){
+        mSurfaceHolder = getHolder();
+        mSurfaceHolder.addCallback(this);
+        setFocusable(true);
+        setKeepScreenOn(true);
+        setFocusableInTouchMode(true);
+    }
+}
